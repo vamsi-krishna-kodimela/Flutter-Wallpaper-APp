fabba80f4f886e1d7dd66b32b9b817020bbe184e
Vamsi Krishna <vamsikrishnakdml@gmail.com>
2024-05-19 10:55:11 +0530

    chore: Refactor wallpaper grid to use GridView.builder

--- lib/views/components/wallpaper_grid.dart
+++ lib/views/components/wallpaper_grid.dart
@@ -5,25 +5,23 @@ class WallpaperGrid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
+    return GridView.builder(
+      itemCount: 1000,
       padding: const EdgeInsets.all(40.0),
-      child: GridView.builder(
-        itemCount: 100,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 4,
-          childAspectRatio: 2 / 3,
-          crossAxisSpacing: 24.0,
-          mainAxisSpacing: 24.0,
-        ),
-        itemBuilder: (_, index) => Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16.0),
-            image: DecorationImage(
-              image: NetworkImage(
-                "https://picsum.photos/seed/${index + 1}/200/300",
-              ),
-              fit: BoxFit.cover,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 4,
+        childAspectRatio: 2 / 3,
+        crossAxisSpacing: 24.0,
+        mainAxisSpacing: 24.0,
+      ),
+      itemBuilder: (_, index) => Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16.0),
+          image: DecorationImage(
+            image: NetworkImage(
+              "https://picsum.photos/seed/${index + 1}/200/300",
             ),
+            fit: BoxFit.cover,
           ),
         ),
       ),
